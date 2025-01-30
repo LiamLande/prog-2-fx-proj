34f56c130ca7da757f4251279a77afb4e50dd993
Alexander Owren Elton <alexander.owren.elton@hotmail.com>
2025-01-30 23:09:09 +0100

    Implement Die and Dice classes with rolling functionality

--- IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/model/Dice.java
+++ IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/model/Dice.java
@@ -1,5 +1,26 @@
 package edu.ntnu.idi.bidata.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dice {
+  private final List<Die> dice;
+
+  public Dice(int numberOfDice) {
+    this.dice = new ArrayList<>();
+    for (int i = 0; i < numberOfDice; i++) {
+      this.dice.add(new Die()); // Add a new Die object for each dice
+    }
+  }
+
+  public int roll() {
+    return dice.stream()
+        .mapToInt(Die::roll)
+        .sum();
+  }
+
+  public int getDie(int dieNumber) {
+    return dice.get(dieNumber).getValue();
+  }
 
 }
--- IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/model/Die.java
+++ IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/model/Die.java
@@ -1,5 +1,17 @@
 package edu.ntnu.idi.bidata.model;
 
-public class Die {
+import java.util.Random;
 
+public class Die {
+  private int lastRolledValue;
+  Random ran = new Random();
+
+  public int roll() {
+    lastRolledValue = ran.nextInt(6) + 1;
+    return lastRolledValue;
+  }
+
+  public int getValue() {
+    return lastRolledValue;
+  }
 }
