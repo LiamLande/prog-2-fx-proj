4b23e28560d071532ef59b9ad7c725096babf3dd
Alexander Owren Elton <alexander.owren.elton@hotmail.com>
2025-02-09 14:41:22 +0100

    Implemented game controll wihtin the BoardGame class

--- IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/model/BoardGame.java
+++ IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/model/BoardGame.java
@@ -1,5 +1,7 @@
 package edu.ntnu.idi.bidata.model;
 
+import edu.ntnu.idi.bidata.model.actions.LadderAction;
+import edu.ntnu.idi.bidata.model.actions.SnakeAction;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,23 +17,76 @@ public class BoardGame {
 
   public void createBoard() {
     board = new Board();
+    // Create 100 tiles for the board
+    for (int i = 0; i < 100; i++) {
+      board.addTile(new Tile(i));
+    }
 
+    setupLaddersAndSnakes();
   }
+
+  private void setupLaddersAndSnakes() {
+    // Ensure all tiles are linked correctly
+    for (int i = 0; i < 100; i++) {
+      board.getTile(i).setNextTile(board.getTile(i + 1));
+      if (i < 99) {
+        board.getTile(i + 1).setPreviousTile(board.getTile(i));
+      }
+    }
+
+    // Ladders (Move Forward)
+    board.getTile(3).setLandAction(new LadderAction("Ladder to 22!", 19)); // 3 -> 22
+    board.getTile(8).setLandAction(new LadderAction("Ladder to 30!", 22)); // 8 -> 30
+    board.getTile(28).setLandAction(new LadderAction("Ladder to 84!", 56)); // 28 -> 84
+    board.getTile(58).setLandAction(new LadderAction("Ladder to 77!", 19)); // 58 -> 77
+
+    // Snakes (Move Backward)
+    board.getTile(16).setLandAction(new SnakeAction("Snake down to 6!", 10)); // 16 -> 6
+    board.getTile(47).setLandAction(new SnakeAction("Snake down to 26!", 21)); // 47 -> 26
+    board.getTile(62).setLandAction(new SnakeAction("Snake down to 18!", 44)); // 62 -> 18
+    board.getTile(87).setLandAction(new SnakeAction("Snake down to 24!", 63)); // 87 -> 24
+  }
+
 
   public void createDice() {
     dice = new Dice(1);
   }
 
   public void play() {
-
+    while (!gameOver()) {
+      for (Player player : players) {
+        currentPlayer = player;
+        int steps = dice.roll();
+        System.out.println(player.getName() + " rolled a " + steps);
+        player.move(steps);
+        System.out.println(player.getName() + " is now on tile " + player.getCurrentTile().getTileId());
+        if (player.getCurrentTile().getTileId() >= 99) {
+          System.out.println(player.getName() + " wins!");
+          return;
+        }
+      }
+    }
   }
 
   public Player getWinner() {
+    for (Player player : players) {
+      if (player.getCurrentTile().getTileId() == 99) {
+        return player;
+      }
+    }
     return null;
+  }
+
+  public boolean gameOver() {
+    for (Player player : players) {
+      if (player.getCurrentTile().getTileId() == 99) {
+        return true;
+      }
+    }
+    return false;
   }
 
   public Board getBoard() {
     return board;
   }
-
 }
