db051014ea17a18bf38808d34febffeb32eb5982
Alexander Owren Elton <alexander.owren.elton@hotmail.com>
2025-04-21 22:19:37 +0200

    Refactor(Die/dice): Improving code and adding javadoc

--- IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/model/Dice.java
+++ IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/model/Dice.java
@@ -2,25 +2,45 @@ package edu.ntnu.idi.bidata.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import edu.ntnu.idi.bidata.exception.InvalidParameterException;
 
+/**
+ * Aggregates multiple Die instances and rolls them together.
+ */
 public class Dice {
   private final List<Die> dice;
 
+  /**
+   * Creates a collection of the given number of dice.
+   * @param numberOfDice must be >0
+   */
   public Dice(int numberOfDice) {
-    this.dice = new ArrayList<>();
+    if (numberOfDice < 1) {
+      throw new InvalidParameterException("Must create at least one die");
+    }
+    dice = new ArrayList<>(numberOfDice);
     for (int i = 0; i < numberOfDice; i++) {
-      this.dice.add(new Die()); // Add a new Die object for each dice
+      dice.add(new Die());
     }
   }
 
+  /**
+   * Rolls all dice and returns the sum of their face values.
+   */
   public int roll() {
     return dice.stream()
         .mapToInt(Die::roll)
         .sum();
   }
 
-  public int getDie(int dieNumber) {
-    return dice.get(dieNumber).getValue();
+  /**
+   * Returns last rolled value of a specific die (0‐based index).
+   */
+  public int getDie(int index) {
+    if (index < 0 || index >= dice.size()) {
+      throw new InvalidParameterException("Die index out of range: " + index);
+    }
+    return dice.get(index).getValue();
   }
-
 }
+
--- IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/model/Die.java
+++ IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/model/Die.java
@@ -2,15 +2,26 @@ package edu.ntnu.idi.bidata.model;
 
 import java.util.Random;
 
+/**
+ * Represents a single six‐sided die.
+ */
 public class Die {
+  private static final int SIDES = 6;
   private int lastRolledValue;
-  Random ran = new Random();
+  private final Random random = new Random();
 
+  /**
+   * Rolls this die, updates and returns the face value (1–6).
+   * @return rolled value
+   */
   public int roll() {
-    lastRolledValue = ran.nextInt(6) + 1;
+    lastRolledValue = random.nextInt(SIDES) + 1;
     return lastRolledValue;
   }
 
+  /**
+   * Returns the last rolled value (undefined before first roll).
+   */
   public int getValue() {
     return lastRolledValue;
   }
