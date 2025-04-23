08ed26d9688a4e59629d820b1e6788b3c41948a2
Alexander Owren Elton <alexander.owren.elton@hotmail.com>
2025-04-23 22:47:32 +0200

    Feat(View/BoardView): Initial UI class containing the board

--- IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/ui/BoardView.java
+++ IDATT2003-Mappe-Boardgame/src/main/java/edu/ntnu/idi/bidata/ui/BoardView.java
@@ -1,4 +1,21 @@
 package edu.ntnu.idi.bidata.ui;
 
-public class BoardView {
+import edu.ntnu.idi.bidata.model.BoardGame;
+import javafx.scene.layout.Pane;
+
+/**
+ * Renders the game board and player tokens.
+ */
+public class BoardView extends Pane {
+  public BoardView(BoardGame game) {
+    setPrefSize(600, 600);
+    // TODO: draw grid, tokens, snakes/ladders
+  }
+
+  /**
+   * Updates the view to reflect current game state.
+   */
+  public void refresh() {
+    // TODO: reposition tokens according to player.getCurrent()
+  }
 }
